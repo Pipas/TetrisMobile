a891194b9639987e2e832c183cc15b19f1e97d45
Pipas_ <19pipas@gmail.com>
2017-06-05 16:05:09 +0100

    Changed game visuals

--- core/src/com/tetris/ui/GameScreen.java
+++ core/src/com/tetris/ui/GameScreen.java
@@ -9,14 +9,20 @@ import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.tetris.game.GameState;
 
+import static java.awt.Color.orange;
+import static java.awt.Color.pink;
+import static java.awt.Color.red;
+import static java.awt.Color.yellow;
+import static javax.sound.midi.ShortMessage.START;
+
 /**
  * Created by Alexandre on 04-05-2017.
  */
 
 public class GameScreen extends ScreenAdapter implements InputProcessor {
     private SpriteBatch batch;
-    private Sprite sprite;
-    private Texture red, blue, green, orange, pink, yellow, teal;
+    private Sprite square, backsprite, infosprite;
+    private Texture blue, green, white, background, info;
     private GameState gameState;
     private int count = 0;
     private int strafeCount = 0;
@@ -25,6 +31,9 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
     private int BASE_SPEED = 30;
     private float FAST_DROP_ZONE;
     private float BLOCK_SIZE;
+    private float HEADER_HEIGHT;
+    private float BOARD_HEIGHT, BOARD_WIDTH;
+    private float START_X, START_Y, START_X_BOARD;
 
     @Override
     public void show(){
@@ -32,48 +41,57 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
 
         gameState = new GameState();
 
-        red = new Texture("redHappySquare.png");
-        blue = new Texture("blueHappySquare.png");
-        orange = new Texture("orangeHappySquare.png");
-        yellow = new Texture("yellowHappySquare.png");
-        green = new Texture("greenHappySquare.png");
-        pink = new Texture("pinkHappySquare.png");
-        teal = new Texture("tealHappySquare.png");
+        white = new Texture("whiteSquare.png");
+        blue = new Texture("blueSquare.png");
+        green = new Texture("greenSquare.png");
+        background = new Texture("gameBackground.png");
+        info = new Texture("infoBackground.png");
+        backsprite = new Sprite(background);
+        infosprite = new Sprite(info);
 
-        BLOCK_SIZE = Gdx.graphics.getWidth() / 10;
         FAST_DROP_ZONE = Gdx.graphics.getHeight() * 0.8f;
+        HEADER_HEIGHT = Gdx.graphics.getWidth()*0.336f;
+        BOARD_HEIGHT = Gdx.graphics.getHeight() - HEADER_HEIGHT;
+        BOARD_WIDTH = 0.704f * BOARD_HEIGHT;
+        START_X_BOARD = (Gdx.graphics.getWidth() - BOARD_WIDTH)/2;
+        BLOCK_SIZE = (BOARD_WIDTH * 0.832f)/10f;
+        START_X = (0.084f * BOARD_WIDTH) + START_X_BOARD;
+        START_Y = 0.059f * BOARD_HEIGHT;
 
         Gdx.input.setInputProcessor(this);
     }
 
     @Override
     public void render(float delta){
-        Gdx.gl.glClearColor(1,1,1,1);
+        Gdx.gl.glClearColor(0.42745f,0.42745f,0.42745f,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 
         batch.begin();
+
+        batch.draw(infosprite, 0, Gdx.graphics.getHeight() - HEADER_HEIGHT, Gdx.graphics.getWidth(), HEADER_HEIGHT);
+        batch.draw(backsprite, START_X_BOARD , 0, BOARD_WIDTH , BOARD_HEIGHT);
 
         for(int x = 0; x < 10; x ++)
         {
             for(int y = 0; y < 15; y++)
             {
                 if(gameState.getDynamicBoard()[y][x] == 'T' || gameState.getDynamicBoard()[y][x] == 't')
-                    sprite = new Sprite(red);
+                    square = new Sprite(white);
                 else if(gameState.getDynamicBoard()[y][x] == 'S' || gameState.getDynamicBoard()[y][x] == 's')
-                    sprite = new Sprite(yellow);
+                    square = new Sprite(white);
                 else if(gameState.getDynamicBoard()[y][x] == 'K' || gameState.getDynamicBoard()[y][x] == 'k')
-                    sprite = new Sprite(orange);
+                    square = new Sprite(blue);
                 else if(gameState.getDynamicBoard()[y][x] == 'A' || gameState.getDynamicBoard()[y][x] == 'a')
-                    sprite = new Sprite(green);
+                    square = new Sprite(green);
                 else if(gameState.getDynamicBoard()[y][x] == 'L' || gameState.getDynamicBoard()[y][x] == 'l')
-                    sprite = new Sprite(pink);
+                    square = new Sprite(green);
                 else if(gameState.getDynamicBoard()[y][x] == 'V' || gameState.getDynamicBoard()[y][x] == 'v')
-                    sprite = new Sprite(blue);
+                    square = new Sprite(blue);
                 else
-                    sprite = new Sprite(teal);
+                    square = new Sprite(white);
 
                 if(gameState.getDynamicBoard()[y][x] != ' ')
-                    batch.draw(sprite, x*BLOCK_SIZE, y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
+                    batch.draw(square, START_X + x*BLOCK_SIZE, START_Y + y*BLOCK_SIZE, BLOCK_SIZE, BLOCK_SIZE);
             }
         }
 
@@ -128,13 +146,11 @@ public class GameScreen extends ScreenAdapter implements InputProcessor {
     public void dispose ()
     {
         batch.dispose();
-        red.dispose();
+        white.dispose();
         blue.dispose();
         green.dispose();
-        yellow.dispose();
-        orange.dispose();
-        pink.dispose();
-        teal.dispose();
+        background.dispose();
+        info.dispose();
     }
 
     @Override
