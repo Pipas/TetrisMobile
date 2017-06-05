1a7b7411866a12b674059f8e9d57234e40066bb5
Pipas_ <19pipas@gmail.com>
2017-06-05 22:15:46 +0100

    Ajusted Main Menu to new Layout

--- core/src/com/tetris/ui/MenuScreen.java
+++ core/src/com/tetris/ui/MenuScreen.java
@@ -8,13 +8,16 @@ import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.graphics.g2d.freetype.FreeTypeFontGenerator;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
 import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
+import com.badlogic.gdx.utils.Align;
 
 /**
  * Created by Alexandre on 04-05-2017.
@@ -22,11 +25,10 @@ import com.badlogic.gdx.scenes.scene2d.utils.SpriteDrawable;
 
 public class MenuScreen extends ScreenAdapter {
 
-    //To delete
-    private Texture menu;
     private SpriteBatch batch;
 
     private Stage stage;
+    private Texture background, top, bot;
     private TextButton startButton, scoreButton;
     private BitmapFont titleFont, buttonFont;
     private Label title;
@@ -40,12 +42,16 @@ public class MenuScreen extends ScreenAdapter {
         stage = new Stage();
         Gdx.input.setInputProcessor(stage);
 
-        generator = new FreeTypeFontGenerator(Gdx.files.internal("XcelsionItalic.ttf"));
+        generator = new FreeTypeFontGenerator(Gdx.files.internal("prstart.ttf"));
         parameter = new FreeTypeFontGenerator.FreeTypeFontParameter();
-        parameter.size = 40;
+        parameter.size = 50;
         titleFont = generator.generateFont(parameter);
-        titleFont.setColor(Color.BLACK);
+        titleFont.setColor(Color.WHITE);
         buttonFont = titleFont;
+
+        background = new Texture("mainMenuBackground.png");
+        top = new Texture("mainMenuTop.png");
+        bot = new Texture("mainMenuBottom.png");
 
         SpriteDrawable spriteDrawable = new SpriteDrawable(new Sprite(new Texture("buttonBackground.png")));
         TextButton.TextButtonStyle ts = new TextButton.TextButtonStyle(spriteDrawable, spriteDrawable, spriteDrawable, buttonFont);
@@ -53,7 +59,7 @@ public class MenuScreen extends ScreenAdapter {
         ts.pressedOffsetY = -3;
 
         startButton = new TextButton("START", ts);
-        scoreButton = new TextButton("HIGHSCORES", ts);
+        scoreButton = new TextButton("HIGH\nSCORES", ts);
         startButton.setPosition(0.2f*GameTetris.VIEWPORT_WIDTH, 0.5f*GameTetris.VIEWPORT_HEIGHT);
         scoreButton.setPosition(0.2f*GameTetris.VIEWPORT_WIDTH, 0.2f*GameTetris.VIEWPORT_HEIGHT);
         startButton.setSize(0.6f*GameTetris.VIEWPORT_WIDTH, 0.2f*GameTetris.VIEWPORT_HEIGHT);
@@ -67,21 +73,27 @@ public class MenuScreen extends ScreenAdapter {
             }
         });
 
-        Label.LabelStyle ls = new Label.LabelStyle(titleFont, Color.BLACK);
-        title = new Label("Tetris Mobile", ls);
-        title.setFontScaleY(2);
-        title.setFontScaleX(1.2f);
-        title.setPosition(0.1f*GameTetris.VIEWPORT_WIDTH, 0.8f*GameTetris.VIEWPORT_HEIGHT);
+        Label.LabelStyle ls = new Label.LabelStyle(titleFont, Color.WHITE);
+        title = new Label("TETRIS\nMOBILE", ls);
+        title.setPosition(0.5f*GameTetris.VIEWPORT_WIDTH - title.getWidth()/2*1.6f, 0.8f*GameTetris.VIEWPORT_HEIGHT);
+        title.setFontScaleY(1.6f);
+        title.setFontScaleX(1.6f);
+
 
         stage.addActor(startButton);
         stage.addActor(scoreButton);
         stage.addActor(title);
-
     }
     @Override
     public void render(float delta){
         Gdx.gl.glClearColor(1,1,1,1);
         Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        batch.begin();
+        batch.draw(background, 0, 0,  Gdx.graphics.getWidth(),  Gdx.graphics.getHeight());
+        batch.draw(top, 0, Gdx.graphics.getHeight() - Gdx.graphics.getWidth()*0.084f,  Gdx.graphics.getWidth(),  Gdx.graphics.getWidth()*0.084f);
+        batch.draw(bot, 0, 0,  Gdx.graphics.getWidth(),  Gdx.graphics.getWidth()*0.084f);
+        batch.end();
 
         stage.act(delta);
         stage.draw();
